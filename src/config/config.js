31509d2facc8e937356d219a1d0e3e8d355f0569
Manu SOdhi <mssodhi@ucdavis.edu>
2018-10-25 17:37:53 -0700

    cleanup config and helpers

--- src/config/config.js
+++ src/config/config.js
@@ -11,19 +11,12 @@ const config = {
     password: process.env.DB_PASSWORD || 'app_user_1234',
     JWT_SECRET: 'ABCD'
   },
-  development: {
-    host: process.env.DB_HOSTNAME,
-    database: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    JWT_SECRET: 'EFG'
-  },
   production: {
     host: process.env.DB_HOSTNAME,
     database: process.env.DB_NAME,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    JWT_SECRET: process.env.JWT_SECRET || 'HIJKLMNOP'
+    JWT_SECRET: process.env.JWT_SECRET || 'EFGH'
   }
 };
 module.exports = { ...SHARED_KEYS, ...config[process.env.NODE_ENV] };
